5d874aa88c404edeb15cdaf7199ce541800d4bf8
Patrick Eriksson <paleite@users.noreply.github.com>
2021-10-27 15:09:30 +0200

    Node 16

--- .ncurc.js
+++ .ncurc.js
@@ -1,5 +1,16 @@
+/** @type {(d: string) => boolean} */
+const hasDateExpired = (d) => Date.now() > new Date(d).getTime();
+
+/** @type {(package: string, date: string) => string[]} */
+const avoidUpdatingPackageUntil = (package, date) =>
+  hasDateExpired(date) ? [] : [package];
+
+const reject = [
+  "@types/node",
+  avoidUpdatingPackageUntil("eslint", "2021-11-15"),
+].flat();
 module.exports = {
   packageManager: "yarn",
   upgrade: true,
-  reject: ["@types/node"],
+  /** @type {string[]} */ reject,
 };
